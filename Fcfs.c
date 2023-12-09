a9e2a537220da2074ea6d2f40144b107ffd28f97
Abhinav U <129955044+06abhinav123@users.noreply.github.com>
2023-12-09 23:43:30 +0530

    Update Fcfs.c

--- Fcfs.c
+++ Fcfs.c
@@ -1,1 +1,14 @@
-
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+  int i,j,n,move,initial,index,tot_headmove=0,rq[100];
+  printf("Enter the no of sequences: ");
+  scanf("%d",&n);
+  printf("\nEnter the sequences : ");
+  for(i=0;i<n;i++)
+    {
+      scanf("%d",&rq[i];
+    }
+  
+}
