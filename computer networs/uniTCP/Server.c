0b57955d79741c3e4bb557d7229bb7aabce636ba
Abhinav U <129955044+abhinavhh@users.noreply.github.com>
2024-07-04 18:27:02 +0530

    Update Server.c

--- computer networs/uniTCP/Server.c
+++ computer networs/uniTCP/Server.c
@@ -8,15 +8,26 @@ void main(){
     int sd,nsd;
     char sendmsg[30],recvmsg[30];
     struct sockaddr_in cliaddr,servaddr;
+
     sd=socket(AF_INET,SOCK_STREAM,0);
+
     servaddr.sin_family=AF_INET;
     servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
     servaddr.sin_port=htons(33345);
+
+
     bind(sd,(struct sockaddr*)&servaddr,sizeof(servaddr));
     int len=sizeof(cliaddr);
+
     listen(sd,5);
+
     nsd=accept(sd,(struct sockaddr*)&cliaddr,&len);
+
     printf("\nReceived String : ");
+
     recv(nsd,recvmsg,20,0);
+
     printf("%s\n",recvmsg);
+    printf("\nEnter the message to send :");
 }
+
