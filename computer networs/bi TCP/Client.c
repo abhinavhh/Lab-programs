e0c1f91df8ee767dda2e88700c77cb84fdf8c37d
Abhinav U <129955044+abhinavhh@users.noreply.github.com>
2024-07-04 18:24:43 +0530

    Update Client.c

--- computer networs/bi TCP/Client.c
+++ computer networs/bi TCP/Client.c
@@ -1,20 +1,40 @@
 #include<stdio.h>
+#include<string.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
-#include<string.h>
-void main(){
-    int csd,len;
-    char sendmsg[30],recvmsg[30];
-    struct sockaddr_in cliaddr,servaddr;
-    csd=socket(AF_INET,SOCK_STREAM,0);
-    servaddr.sin_family=AF_INET;
-    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-    servaddr.sin_port=htons(33345);
-    connect(csd,(struct sockaddr*)&servaddr,sizeof(servaddr));
-    printf("\nEnter the message to send :");
-    fgets(sendmsg,20,stdin);
-    len=strlen(sendmsg);
-    sendmsg[len-1]='\0';
-    send(csd,sendmsg,20,0);
+
+void main() {
+
+	/*CLIENT PROGRAM*/
+
+	int csd, len;
+	char sendmsg[30], recvmsg[20];
+	struct sockaddr_in cliaddr, servaddr;
+	
+	//	SOCKET
+	csd = socket(AF_INET, SOCK_STREAM, 0);
+	
+	/**/
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(33345);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	 /**/
+	//	CONNECT
+	connect(csd, (struct sockaddr*)&servaddr, sizeof(servaddr));
+
+	do{
+		//	SEND
+		printf("Client: ");
+		fgets(sendmsg, 30, stdin);
+		len = strlen(sendmsg);
+		sendmsg[len-1] = '\0';
+		send(csd, sendmsg, 20, 0);
+
+		//	RECEIVE	
+		recv(csd, recvmsg, 20, 0);
+		printf("Server: %s\n", recvmsg);
+	}while(strncmp(sendmsg,"bye",3)==0);
+	//	CLOSE
+	// int x = close(csd);
 }
